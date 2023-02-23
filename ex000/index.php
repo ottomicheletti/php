0da24154f371294c314ac077e7a67dc0b23ca245
Otto Micheletti <michelettiotto@gmail.com>
2023-02-23 09:02:55 -0300

    feat: :art: Creation of 'ex000' folder to organize previous exercise done.

--- ex000/index.php
+++ ex000/index.php
@@ -1,14 +1,17 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
+
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>Hello, World!</title>
 </head>
+
 <body>
-  <?php
-    echo "Hello, world!"
-  ?>
+    <?php
+      echo "Olá, Mundo! \u{1F30E}";
+    ?>
 </body>
+
 </html>
