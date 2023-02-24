d91608a83fdee331ffe0b22742940b6faab1da1a
Otto Micheletti <michelettiotto@gmail.com>
2023-02-24 10:36:50 -0300

    feat: :sparkles: Learning about chr() and ord() built-in functions.

---
+++ ex011/chr_e_ord.php
@@ -0,0 +1,10 @@
+<?php
+  $letra1 = chr(65);
+  // Converte o código ASCII em sua letra correspondente.
+  echo "A letra de código 65 (ASCII) é: $letra1.<br>";
+
+  $letra2 = "C";
+  $codigo = ord($letra2);
+  // Converte uma letra em seu código ASCII.
+  echo "A letra C possui o código $codigo em ASCII";
+?>
