9185bfcd7a3001b1942fcb03a750dc14c9f3cf22
Otto Micheletti <michelettiotto@gmail.com>
2023-02-24 10:40:42 -0300

    feat: :sparkles: Learning about strtolower() built-in function.

---
+++ ex011/strtolower.php
@@ -0,0 +1,7 @@
+<?php
+  $texto = "Otto Micheletti";
+  $texto2 = strtolower($texto);
+  // Converte todos os caracteres em maiúsculo para minúsculo.
+
+  echo "O texto era $texto e depois de strtolower() é $texto2.";
+?>
