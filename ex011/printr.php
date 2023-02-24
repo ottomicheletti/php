de65d1d5a77f4d6164e187d116ea0303b715be3f
Otto Micheletti <michelettiotto@gmail.com>
2023-02-24 09:09:57 -0300

    feat: :sparkles: Learning how to use print_r() built-in function and it's alternatives as var_dump() and var_export().

---
+++ ex011/printr.php
@@ -0,0 +1,10 @@
+<?php
+  $x[0] = 4;
+  $x[1] = 3;
+  $x[2] = 8;
+
+  // Imprime o vetor e os valores de seus respectivos indices. Outras alternativas: var_dump() e var_export()
+  print_r($x);
+  // var_dump($x);
+  // var_export($x);
+?>
