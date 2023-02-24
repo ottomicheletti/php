2cc7934cad9106809348e5071469272e28b14506
Otto Micheletti <michelettiotto@gmail.com>
2023-02-24 11:08:07 -0300

    feat: :sparkles: Learning about substr_count() built-in function.

---
+++ ex011/substr_count.php
@@ -0,0 +1,7 @@
+<?php
+  $texto = "Estou aprendendo PHP no Curso em Vídeo de PHP";
+  $contador = substr_count($texto, "PHP");
+  // Conta as instâncias da palavra PHP dentro do texto.
+
+  echo "No texto '$texto', a palavra PHP foi encontrada $contador vezes."
+?>
