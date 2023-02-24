f4408e6622b4b469dde83ac69fb51d5ba8c7c8b1
Otto Micheletti <michelettiotto@gmail.com>
2023-02-24 11:21:59 -0300

    feat: :sparkles: Learning about str_replace() built-in function.

---
+++ ex011/str_replace.php
@@ -0,0 +1,6 @@
+<?php
+  $texto = "Gosto de estudar Matemática!!!";
+  $novoTexto = str_replace("Matemática", "PHP", $texto);
+
+  echo "A frase era: $texto. Após o str_replace('Matemática', 'PHP', \$texto), ficou assim: $novoTexto"
+?>
