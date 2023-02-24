0eaa5b213c33d50841868d7823ddf8c982cf3578
Otto Micheletti <michelettiotto@gmail.com>
2023-02-24 11:09:11 -0300

    feat: :sparkles: Learning about substr() built-in function.

---
+++ ex011/substr.php
@@ -0,0 +1,8 @@
+<?php
+  $texto = "Otto Micheletti";
+  $sub = substr($texto, 0, 4);
+  /* Retorna o fragmento da string correspondente aos indices informados,
+  sendo que o 2º argumento é a posição inicial e o 3º argumento, onde a função deve parar. */
+
+  echo "O texto é '$texto' e a substring encontrada entre as posições 0 e 4 é $sub."
+?>
