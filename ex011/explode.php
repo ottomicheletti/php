6b55616874823e2c3132eab389ff6ebbfa83afda
Otto Micheletti <michelettiotto@gmail.com>
2023-02-24 10:21:16 -0300

    feat: :sparkles: Learning about explode() built-in function.

---
+++ ex011/explode.php
@@ -0,0 +1,7 @@
+<?php
+  $texto = "Lorem ipsum dolor sit amet consectetur adipisicing elit.";
+
+  $vetor = explode(" ", $texto);
+  // Separa a string por palavras em um array onde o 1º argumento da função define o separador.
+  print_r($vetor);
+?>
